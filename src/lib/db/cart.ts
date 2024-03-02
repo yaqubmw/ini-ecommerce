2c47ce21f4db9730a7a96737bcdd83f3f468d309
Yaqub Muflih Wiraatmaja <yaqubmw2@gmail.com>
2024-03-02 15:40:36 +0700

    fix: authOptions in route.ts

--- src/lib/db/cart.ts
+++ src/lib/db/cart.ts
@@ -2,7 +2,7 @@ import { cookies } from "next/headers";
 import prisma from "./prisma";
 import { Cart, CartItem, Prisma } from "@prisma/client";
 import { getServerSession } from "next-auth";
-import { authOptions } from "@/app/api/auth/[...nextauth]/route";
+import authOptions from "@/app/api/auth/[...nextauth]/options";
 
 export type CartWithProducts = Prisma.CartGetPayload<{
   include: { items: { include: { product: true } } };
